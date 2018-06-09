cd3e7929a6f8c960721ef23c4e9ce20d7f43e4f0
Timothy Brathwaite <timothyb0912@gmail.com>
2018-06-09 15:21:09 -0700

    Amended data fetching script to work in project folder structure.

--- src/data/get_car_data.R
+++ src/data/get_car_data.R
@@ -1,13 +1,11 @@
-# Set the current working directory
-setwd("~/Documents/model_checking/code/")
-
 # Load the mlogit package
 library(mlogit)
 
 # Load the Car dataset
 data(Car)
 
+# Declare a path where the data will be saved
+data_path = '../../data/raw/car_wide_format.csv'
+
 # Write the Car dataset to file as a wide-format CSV
-write.csv(Car,
-          file='../data/car_wide_format.csv',
-          row.names = FALSE)
+write.csv(Car, file=data_path, row.names = FALSE)
