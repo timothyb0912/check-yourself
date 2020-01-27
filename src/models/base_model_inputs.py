eb622a46171a07de8029aef1f15c48dcb0f0e33e
Timothy Brathwaite <timothyb0912@gmail.com>
2020-01-26 16:41:15 -0800

    Initial placeholder for the input class for the mixed logit models.

---
+++ src/models/base_model_inputs.py
@@ -0,0 +1,9 @@
+"""
+Contains classes for packaging inputs to the forward functions of the various
+pytorch models for the project.
+"""
+
+
+class InputMixl(object):
+    def __init__(self):
+        raise NotImplementedError()
