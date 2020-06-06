22911f78fc01a03c3a3237fe0ff7232c10e7b4b8
Timothy Brathwaite <timothyb0912@gmail.com>
2020-06-05 18:19:09 -0700

    Notebook for testing MIXLB

---
+++ notebooks/miscellaneous/_18-tb-test-mixlb.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+import sys
+
+sys.path.insert(0, '../../')
+import src.models.mixlb as mixlb
+# -
+mixl_model = mixlb.MIXLB()
