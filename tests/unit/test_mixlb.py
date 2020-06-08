6ad89d53fa0ba132801890058c72e0cb97b97ff3
Timothy Brathwaite <timothyb0912@gmail.com>
2020-06-08 16:57:12 -0700

    [WIP] Adding unit tests of _get_generated_coefs.

--- tests/unit/test_mixlb.py
+++ tests/unit/test_mixlb.py
@@ -2,7 +2,6 @@
 Tests the MIXLB class to ensure it is constructed correctly and that all
 methods work as expected.
 """
-import sys
 import unittest
 
 import torch
@@ -35,4 +34,31 @@ class InputMixlTests(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             func('fake_column')
+        return None
+
+    def test_get_generated_coefs(self):
+        """
+        Ensures expected creation of heterogeneous coefficients.
+        """
+        model = MIXLB()
+        model.double()
+        func = model._get_generated_coefs
+        func_args =\
+            [('luggage_space',
+              7,
+              torch.tensor(2),
+              torch.tensor([0.125, 0.25, 0.5]).double()),
+             ('range_over_100',
+              1,
+              torch.tensor(3),
+              torch.from_numpy((np.log([7.25, 7.5, 8]) - 1) / 3).double()
+             ),
+            ]
+        expected_results =\
+            [torch.tensor([7.25, 7.5, 8]).double(),
+             torch.tensor([7.25, 7.5, 8]).double()]
+
+        for args, expected_result in zip(func_args, expected_results):
+            func_result = func(*args)
+            self.assertTrue(torch.allclose(expected_result, func_result))
         return None
